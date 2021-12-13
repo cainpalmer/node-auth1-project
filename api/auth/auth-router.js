4d3e3399803b47fa90e0f796fd250eeff48871dd
cainpalmer <cain.a.palmer@gmail.com>
2021-12-13 17:26:21 -0500

    completed post new user name and password with hash

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,6 +1,9 @@
 // Require `checkUsernameFree`, `checkUsernameExists` and `checkPasswordLength`
 // middleware functions from `auth-middleware.js`. You will need them here!
 const router = require('express').Router()
+const bcryptjs = require('bcryptjs')
+const bcrypt = require('bcryptjs')
+const User = require('../users/users-model')
 const {checkUsernameFree, checkUsernameExists, checkPasswordLength} = require('./auth-middleware')
 
 /**
@@ -26,7 +29,13 @@ const {checkUsernameFree, checkUsernameExists, checkPasswordLength} = require('.
   }
  */
   router.post('/register', checkPasswordLength, checkUsernameFree, (req, res, next) => {
-    res.json('register')
+    const {username, password} = req.body
+    const hash = bcrypt.hashSync(password, 8)
+    User.add({username, password: hash})
+    .then(saved => {
+      res.status(201).json(saved)
+    })
+    .catch(next)
   })
 
 
